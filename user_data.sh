139d63fc549567a53a4b27a63639768869ed4fc1
lontok <lontok@users.noreply.github.com>
2021-11-09 14:11:14 -0800

    Create user_data.sh

---
+++ user_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# updates installed packages
+yum update -y
+# installs Apache web server
+yum install -y httpd
+# starts Apache web server
+systemctl start httpd
+# enables Apache web server to start at boot
+systemctl enable httpd
+# overwrites index page
+echo "Hello from $(hostname)" > /var/www/html/index.html
